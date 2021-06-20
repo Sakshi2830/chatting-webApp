a7e960b151cb9f559057b195e9a04d9283f047f1
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-06-21 02:18:34 +0530

    creating chatWindow

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Sidebar.css'
 import SearchIcon from "@material-ui/icons/Search"
+import { BorderColorOutlined } from '@material-ui/icons'
+import { IconButton } from '@material-ui/core'
+
 
 const Sidebar = () => {
     return (
@@ -10,6 +13,9 @@ const Sidebar = () => {
                     <SearchIcon className="sidebar__searchicon" />
                     <input placeholder="Search" className="sidebar__input"></input>
                 </div>
+                <IconButton variant=" outlined" id="sidebar__button">
+                <BorderColorOutlined />
+                </IconButton>
             </div>
             <div className="sidebar__thread"></div>
             <div className="sidebar__bottom"></div>
